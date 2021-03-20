594566cfa1723487863f996bc434156841b19bb4
Seanbon0611 <seand0611@gmail.com>
2021-03-19 18:47:47 -0700

    added sstate to collect users gender

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import LandingPage from "./pages/LandingPage";
 import { Route, Switch } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route component={ErrorPage} />
       </Switch>
+      <Footer />
     </div>
   );
 }
