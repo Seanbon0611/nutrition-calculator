d215023b3266f5263a16a4f146b8e8fc5cff9ff4
Seanbon0611 <seand0611@gmail.com>
2021-03-20 20:30:58 -0700

    added next and prev buttons to user Goals form

--- src/components/UserGoalsForm.jsx
+++ src/components/UserGoalsForm.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const UserGoalsForm = () => {
+const UserGoalsForm = ({ next, prev, setGoal }) => {
   return (
     <div>
       <h1>Goals</h1>
+      <button onClick={next}>Continue</button>
+      <button onClick={prev}>Back</button>
     </div>
   );
 };
