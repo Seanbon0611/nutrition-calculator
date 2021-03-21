6976c1c9550fe0bfaae9e341ff4fc6c9880aebd3
Seanbon0611 <seand0611@gmail.com>
2021-03-20 20:38:27 -0700

    added form options for userGoals

--- src/components/UserGoalsForm.jsx
+++ src/components/UserGoalsForm.jsx
@@ -4,6 +4,19 @@ const UserGoalsForm = ({ next, prev, setGoal }) => {
   return (
     <div>
       <h1>Goals</h1>
+      <h3>Next, let's take note of your goals!</h3>
+      <p>Options:</p>
+      <ul>
+        <li>Build Muscle(Hypertrophy) / Build Strength</li>
+        <li>Maintainance(Just want to maintain current weight </li>
+        <li>Fat-Loss</li>
+      </ul>
+      <select onChange={(e) => setGoal(e.target.value)}>
+        <option value="">-</option>
+        <option value="lose">Fat-Loss</option>
+        <option value="gain">Hypertrophy/Strength</option>
+        <option value="maintain">Maintainance</option>
+      </select>
       <button onClick={next}>Continue</button>
       <button onClick={prev}>Back</button>
     </div>
