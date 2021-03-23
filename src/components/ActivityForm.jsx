b5a325d4821e43a0e6bd4a692bbfe1d397cfe8fc
Seanbon0611 <seand0611@gmail.com>
2021-03-22 19:33:00 -0700

    passed in handleSubmit function into activity form

--- src/components/ActivityForm.jsx
+++ src/components/ActivityForm.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 
-const ActivityForm = ({ setDaysActive, setActivityLevel }) => {
+const ActivityForm = ({
+  setDaysActive,
+  setActivityLevel,
+  next,
+  prev,
+  handleSubmit,
+}) => {
   return (
     <div>
       <h1>Actvity</h1>
       <label>
         How many days are you active per week?
-        <input
-          type="number"
-          onChange={(e) => setActivityLevel(e.target.value)}
-        />
+        <input type="number" onChange={(e) => setDaysActive(e.target.value)} />
       </label>
       <p>Light desription</p>
       <p>Moderate desription</p>
       <p>Intense desription</p>
-      <label>
-        <select onChange={(e) => setDaysActive(e.target.value)}>
-          <option value="light">Light</option>
-          <option value="moderate"></option>
-          <option value="intense"></option>
-        </select>
-      </label>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <select onChange={(e) => setActivityLevel(e.target.value)}>
+            <option>-</option>
+            <option value="light">Light</option>
+            <option value="moderate">Moderate</option>
+            <option value="intense">Intense</option>
+          </select>
+        </label>
+        <button onClick={prev}>Back</button>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
